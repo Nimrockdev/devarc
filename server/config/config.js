a05d3b91d228bec8c7160ef362e55b7d74432a8a
Nimrockdev <nimrockdev@gmail.com>
2020-05-22 13:39:58 +0200

    update time token

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ const config = {
     dev: process.env.NODE_ENV || 'prod',
     port: process.env.PORT || 3000,
     urlDB: process.env.URL_DB || process.env.MONGO_URI,
-    TOKEN_EXPIRATION: process.env.TOKEN_EXPIRATION || 3600
+    TOKEN_EXPIRATION: process.env.TOKEN_EXPIRATION || 3600000
 }
 
 module.exports = config;
