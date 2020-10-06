d24bd204586639414f53fda421d35836536c0b87
Nimrockdev <nimrockdev@gmail.com>
2020-10-06 14:12:45 +0200

    get and post tax

--- server/routes/tax.js
+++ server/routes/tax.js
@@ -3,7 +3,7 @@ const Tax = require('../models/tax');
 
 const app = express();
 
-
+//Only admin
 app.get('/tax', (req, res) =>{
     let Taxes;
     Tax.find({})
@@ -22,7 +22,38 @@ app.get('/tax', (req, res) =>{
     })
 });
 
+app.post('/tax',(req, res) =>{
 
+    let body = req.body;
+
+    console.log(body)
+
+    let tax = new Tax({
+        codeCountry : body.codeCountry,
+        code        : body.code,
+        value       : body.value,
+        description : body.description,
+        active      : body.active
+    });
+   
+
+    tax.save( (err,taxDB) =>{
+      
+        if (err) {
+
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        
+        res.json({
+            ok: true,
+            tax: taxDB
+        });
+    });
+
+});
 
 
 module.exports= app;
