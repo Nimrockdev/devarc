03a221ae5a0d6f991f639bf767f61bd6961e6188
Nimrockdev <nimrockdev@gmail.com>
2021-02-04 10:31:49 +0100

    add server Coors

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use(require('./routes/index'));
 
 mongoose
