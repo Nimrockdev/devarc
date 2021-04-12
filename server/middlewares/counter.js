3c562d2cff704c2c25f5d5fa7f555a804da1e552
Nimrockdev <nimrockdev@gmail.com>
2021-04-12 10:32:43 +0200

    Add visitor counter
    - Add Visitor counter for all request, local variable

---
+++ server/middlewares/counter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+let app = express();
+
+app.use((req, res, next)=>{
+    app.locals.counter = (++app.locals.counter || 1);
+    console.log(`Visitor counter ${app.locals.counter}`);
+    next();
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/config');
-
+const counter = require('./middlewares/counter');
 
 const app = express();
 
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
 });
 
 
+app.use(counter);
 app.use(require('./routes/index'));
 
 mongoose
